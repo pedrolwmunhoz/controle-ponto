16404fd6a8ece26bbbe535019a2f779e910e50f8
Pedro Munhoz <phcmunhoz@rd.com.br>
2023-03-10 14:53:56 -0300

    make it better

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ require('dotenv').config
 const cors = require('cors');
 
 const app = express();
-app.use(cors({
-  origin: 'http://localhost:3000'
-}));
+
+app.use(express.json());
+app.use(cors());
 
 
 const pool = new Pool({
